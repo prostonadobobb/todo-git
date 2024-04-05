2d7f3dab794278772b76cf093eacb52f0cb5cb8d
prostonadobobb <prostonadobobb@gmail.com>
2024-04-05 21:43:33 +0300

    add GET request

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,137 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.scss";
+import { RepoInput } from "./components/RepoInput";
+import { Octokit } from "@octokit/core";
 
-function App() {
+export const App: React.FC = () => {
+  const [issues, setIssues] = useState<any[]>([]);
+  const [error, setError] = useState(false);
+
+  const token = new Octokit({
+    auth: "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",
+  });
+
+  const handleRepoSubmit = async (owner: string, repo: string) => {
+    token
+      .request(`GET /repos/${owner}/${repo}/issues`, {
+        owner: "OWNER",
+        repo: "REPO",
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+        state: "all",
+      })
+      .then((response) => {
+        const data = response.data;
+        setIssues(data);
+      })
+      .catch((error) => {
+        console.error("Помилка запиту до GitHub API:", error);
+      });
+  };
+
+  console.log(issues);
+
+  const prepharedTodo = issues.filter((issue) => issue.state === "open");
+  const preharedInProgress = issues.filter((issue) => issue.state && issue.assignee)
+  const preharedClosed = issues.filter((issue) => issue.state === "closed")
+
+  console.log(prepharedTodo);
+
+  function calculateDateDifferent(dateCreated: string) {
+    const createdAt = new Date(dateCreated);
+    const currentDate = new Date();
+
+    const differenceInMilliseconds =
+      currentDate.getTime() - createdAt.getTime();
+    const differenceInDays = Math.floor(
+      differenceInMilliseconds / (1000 * 3600 * 24)
+    );
+
+    return differenceInDays;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App__container">
+        <div className="input">
+          <RepoInput onSubmit={handleRepoSubmit} setError={setError} />
+          {error && <div className="input__error">Не вірний URL!!!</div>}
+        </div>
+
+        <div className="table">
+          <div className="table__column">
+            <div className="table__column--title">ToDo</div>
+
+            {prepharedTodo.map((todo) => (
+              <div className="card">
+                <div className="card__row">
+                  <div className="card__number">{todo.number}</div>
+                  <div className="card__status">{todo.state}</div>
+                  <div className="card_update">
+                    {`${calculateDateDifferent(todo.created_at)} days ago`}
+                  </div>
+                </div>
+
+                <div className="card__row">
+                  <div className="card__type">{todo.user.type}</div>
+                  <span>|</span>
+                  <div className="card__comments">{`Comments: ${todo.comments}`}</div>
+                </div>
+              </div>
+            ))}
+
+          </div>
+
+          <div className="table__column">
+            <div className="table__column--title">In Progress</div>
+
+            {preharedInProgress.map((todo) => (
+              <div className="card">
+                <div className="card__row">
+                  <div className="card__number">{todo.number}</div>
+                  <div className="card__status">{todo.state}</div>
+                  <div className="card_update">
+                    {`${calculateDateDifferent(todo.created_at)} days ago`}
+                  </div>
+                </div>
+
+                <div className="card__row">
+                  <div className="card__type">{todo.user.type}</div>
+                  <span>|</span>
+                  <div className="card__comments">{`Comments: ${todo.comments}`}</div>
+                </div>
+              </div>
+            ))}
+
+          </div>
+
+          <div className="table__column">
+            <div className="table__column--title">Done</div>
+
+            {preharedClosed.map((todo) => (
+              <div className="card">
+                <div className="card__row">
+                  <div className="card__number">{todo.number}</div>
+                  <div className="card__status">{todo.state}</div>
+                  <div className="card_update">
+                    {`${calculateDateDifferent(todo.created_at)} days ago`}
+                  </div>
+                </div>
+
+                <div className="card__row">
+                  <div className="card__type">{todo.user.type}</div>
+                  <span>|</span>
+                  <div className="card__comments">{`Comments: ${todo.comments}`}</div>
+                </div>
+              </div>
+            ))}
+
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/RepoInput.tsx
+++ src/components/RepoInput.tsx
@@ -1,17 +1,48 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './RepoInput.scss';
+import { Button, Form } from 'react-bootstrap';
 
-export const RepoInput = () => {
-  const [url, setUrl] = useState('')
+interface RepoInputProps  {
+  onSubmit: (url: string, repo: string) => void,
+  setError: (error: boolean) => void,
+}
+
+export const RepoInput: React.FC<RepoInputProps> = ({ onSubmit, setError }) => {
+  const [url, setUrl] = useState('');
+  
+  const handleButtonClick = () => {
+    setError(false);
+    const repoCustomer = url
+      .replace('https://github.com/', '')
+      .split('/')
+      .filter(item => item !== '');
+
+    if (repoCustomer.length === 2) {
+      const [owner, repo] = repoCustomer;
+      onSubmit(owner, repo);
+    } else {
+      setError(true);
+    }
+  };
 
   return (
-    <div className="repo">
-      <input 
-        type="text"
-        placeholder="Введіть URL репозиторію"
-        className="repo__input" 
-        value={url}
-      />
+    <div className="input__container">
+      <Form.Group className="input">
+      <Form.Label htmlFor="input" className="input__label">https://github.com/riktar/jkanban</Form.Label>
+        <Form.Control
+          id="input"
+          type="text"
+          placeholder="Введіть URL репозиторію"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <Form.Label htmlFor="input" className="input__label">https://github.com/prostonadobobb/digital-proj</Form.Label>
+        
+      </Form.Group>
+      <div className="button-group">
+        <Button variant="primary" onClick={handleButtonClick}>Завантажити</Button>
+      </div>
     </div>
   );
 };
